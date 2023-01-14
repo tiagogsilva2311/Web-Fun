ddee78270f9bf7ef7a145f5374f5afdc6446a1b0
tiagogsilva2311 <120690981+tiagogsilva2311@users.noreply.github.com>
2023-01-14 12:31:28 -0500

    Add files via upload

---
+++ Button Clicker/script.js
@@ -0,0 +1,7 @@
+function signin () {document.getElementById("login").innerHTML = "Logout";
+}
+
+function vanish() { const element = document.getElementById("add-definition");
+element.remove()}
+
+function like() {alert("ninja was liked")}
